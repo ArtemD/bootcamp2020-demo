a09b65835629bde8e01683871308843bf3c61d56
Artem <artem.daniliants@lumolink.com>
2020-11-03 22:39:20 +0200

    Heroku issues v8

--- bootcamp/wsgi.py
+++ bootcamp/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.bootcamp.settings")
 application = get_wsgi_application()
