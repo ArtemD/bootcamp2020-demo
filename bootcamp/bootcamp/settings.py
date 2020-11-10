908d11b93aa6f1bc722cfe143b075729863c93a2
Artem <artem.daniliants@lumolink.com>
2020-11-10 19:59:15 +0200

    Once more with memcache ><

--- bootcamp/bootcamp/settings.py
+++ bootcamp/bootcamp/settings.py
@@ -149,33 +149,28 @@ else:
 # CACHE_MIDDLEWARE_SECONDS tells server time that cache should be considered valid
 # If you want to clear cache run: heroku run python manage.py clear_cache
 if os.environ.get('DB_URL'):
-    def get_cache():
-        try:
-            servers = os.environ['MEMCACHIER_SERVERS']
-            username = os.environ['MEMCACHIER_USERNAME']
-            password = os.environ['MEMCACHIER_PASSWORD']
-            return {
-            'default': {
-                'BACKEND': 'django_bmemcached.memcached.BMemcached',
-                # TIMEOUT is not the connection timeout! It's the default expiration
-                # timeout that should be applied to keys! Setting it to `None`
-                # disables expiration.
-                'TIMEOUT': None,
-                'LOCATION': servers,
-                'OPTIONS': {
+    servers = os.environ['MEMCACHIER_SERVERS']
+    username = os.environ['MEMCACHIER_USERNAME']
+    password = os.environ['MEMCACHIER_PASSWORD']
+
+    CACHES = {
+        'default': {
+            # Use django-bmemcached
+            'BACKEND': 'django_bmemcached.memcached.BMemcached',
+
+            # TIMEOUT is not the connection timeout! It's the default expiration
+            # timeout that should be applied to keys! Setting it to `None`
+            # disables expiration.
+            'TIMEOUT': None,
+
+            'LOCATION': servers,
+
+            'OPTIONS': {
                 'username': username,
                 'password': password,
-                }
             }
-            }
-        except:
-            return {
-            'default': {
-                'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
-                }
-            }
-
-    CACHES = get_cache()
+        }
+    }
 
     MIDDLEWARE += (
         'django.middleware.cache.UpdateCacheMiddleware',
