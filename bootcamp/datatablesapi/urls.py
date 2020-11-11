dc5f3f9dec3d8d15c9d26337e6cae92b5be55fe1
Artem <artem.daniliants@lumolink.com>
2020-11-11 10:46:17 +0200

    Typo

--- bootcamp/datatablesapi/urls.py
+++ bootcamp/datatablesapi/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 from django.views.decorators.cache import cache_page
 
-
 urlpatterns = [
     url(r'/', cache_page(60 * 60)(views.CompanyJson.as_view())),
 ]
